00d075318986f2adaeedae8d21bff47ad3aae021
Alexander Guizar <aguizar93@gmail.com>
2022-02-28 21:35:39 -0600

    - Added the fxn to hide the current word.

--- main.py
+++ main.py
@@ -2,15 +2,52 @@ import json
 import random
 
 # Load the words from the word bank json file and return the array.
-def loadWords():
+# Choose a random word in the dictionary.
+def loadRandWord():
     with open('word-bank.json') as file:
         data = json.load(file)
-    return data
+    return random.choice(data)
+        
+# Change all the letters in the word to underscores and return that.
+def hideWord(word):
+    hiddenWord = ""
+    for _ in word:
+        hiddenWord += '_'
+    return hiddenWord
 
-# Select a random word to be chosen for 
-def selectRandWord(words):
+# Process the letter in the word and show the correctly guessed letter on the hidden string.
+def processCharInWord(letter, hiddenWord):
     pass
 
 
-if __name__ == '__main__':
-    pass
+    # Top level code
+    if __name__ == '__main__':
+        user_input = 'Y'
+
+        while user_input == 'Y':
+            print('\nWelcome to the game of Hangman. You will have 4 lives to guess the correct word.')
+            
+            user_lives = 6
+
+            guessed_so_far = list()
+                
+            actual_word = loadRandWord()
+
+            hiddenWord = hideWord(actual_word)
+
+            while user_lives != 0:
+
+                # Ask user to guess a letter
+                print(f'The hidden word: {hiddenWord}')
+                user_guess = input('Please guess a letter in the word: ').upper().strip()
+                
+                # Register the letter and see if it's in the word
+                if user_guess in actual_word or user_guess not in guessed_so_far:
+                    processCharInWord(user_guess, hiddenWord) 
+                elif user_guess in guessed_so_far:
+                    print(f'You have alread guessed {user_guess}')
+                else:
+                    print(f'The letter {user_guess} is not in the word.')
+                    user_lives -= 1
+                    guessed_so_far.append(user_guess)
+                # If it is, show the letters in the word and show the letter guessed so far.
